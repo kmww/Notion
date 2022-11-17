5d12676780ce5273229c75594165b52d7d929ec0
kmww <kmw9708@gmail.com>
2022-11-17 16:00:42 +0900

    vercel 배포

---
+++ src/utils/api.js
@@ -0,0 +1,18 @@
+export const request = async (url, option = {}) => {
+  try {
+    const res = await fetch(`${API_PREFIX}${url}`, {
+      ...option,
+      headers: {
+        "content-Type": "application/json",
+        "x-username": "minwoo",
+      },
+    });
+
+    if (res.ok) {
+      return await res.json();
+    }
+    throw new Error("API 처리 오류");
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
